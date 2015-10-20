fc52716dd2e23084e3bf971986ee008e15b8efd4
Max <max@mklingmann.com>
2015-10-20 12:46:28 +0200

    added comments, to why the confiurguration is not tested by unittests

---
+++ tests/test_configuration.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+from unittest import TestCase
+from wordspot.configuration import ArgumentListener
+
+# Placeholder for tests later
+class ConfigurationTestCase(TestCase):
+    def test_anything(self):
+        self.assertTrue(True)
+
+
--- wordspot/configuration.py
+++ wordspot/configuration.py
@@ -6,7 +6,11 @@ from argparse import ArgumentParser
 configLogger = logging.getLogger('Configuration')
 
 
-class Configuration(object):
+class Configuration(object):  # pragma: no cover
+    """
+    This class sets the initial configuration of WordSpot, such as activating verbose mode.
+    This is not unit-tested, since arguments to parse can't be added to the tests
+    """
     def __init__(self):
         self.arg_parser = self.setup_arg_parser()
         self.setup_arg_options()
@@ -27,7 +31,11 @@ class Configuration(object):
                                      help="Activates verbose mode (DEBUG-logging)", action="store_true")
 
 
-class ArgumentListener(object):
+class ArgumentListener(object):  # pragma: no cover
+    """
+    This class handles the functionalities for the given arguments and options via command-line.
+    This class is not unit-tested, since the argument parser fails on unittests, since no arguments get passed.
+    """
     def __init__(self, arg_parser):
         self.arg_parser = arg_parser
         self.options = self.arg_parser.parse_args()
