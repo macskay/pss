e64420e754f454254bbfc7fefbd7498c60011937
Max Klingmann <max@mklingmann.com>
2015-10-28 00:00:05 +0100

    - Added logging, fixed a typo

--- wordspot/svg.py
+++ wordspot/svg.py
@@ -1,20 +1,28 @@
 # -*- encoding: utf-8 -*-
-
+from logging import getLogger
 from os.path import isfile
 
 from external.elka_svg import parse
 
+svg_logger = getLogger("SvgHandler")
+
 
 class SvgHandler(object):
     def __init__(self, path, infix=""):
+        svg_logger.info("SVG-Handler started")
         """
         :param path: Path to the SVG-file (required)
         :param infix: Infix to look for in svg-file (default: "")
         :raise FileNotFoundError: This is raised, when an invalid svg file is passed.
         """
         if not isfile(path):
+            svg_logger.error("Path to SVG-File invalid!")
             raise FileNotFoundError
-        self.names, self.symbol_groups = zip(*parse(infix, path))
 
-    # TODO: add thinner (from skimage.morphology import merdial_axis)
+        svg_logger.info("Opening SVG-File at [%s]", path)
+        self.names, self.symbol_groups = zip(*parse(infix, path))
+        svg_logger.info("SVG-File successfully loaded. (%d names, %d symbol-groups)",
+                        len(self.names), len(self.symbol_groups))
+
+    # TODO: add thinner (from skimage.morphology import medial_axis)
     # TODO: add GUI class to show image (reference this class, calls pathhs to create image on QtApplicaiton)
