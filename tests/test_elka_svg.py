91dcc08f2c775637062a4ad32d31289f53fe24f8
Max Klingmann <max@mklingmann.com>
2015-10-28 00:05:33 +0100

    - skipIf added, since Travis-CI cannot make this test pass, since he does not have any .svg-files

--- tests/test_elka_svg.py
+++ tests/test_elka_svg.py
@@ -1,14 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 # Module under test
-from unittest import TestCase
-from os.path import join, dirname, abspath
+from unittest import TestCase, skipIf
+from os.path import join, dirname, abspath, isfile
 
 from external.elka_svg import parse
 
 FILE_LOCATION = dirname(abspath(__file__))
 
 
+@skipIf(not isfile(join(FILE_LOCATION, "..", "resources", "grouped_VAT_09671_Rs_SJakob.svg")), "Travis-CI has no SVGs")
 class ElkaSvgTestCase(TestCase):
     def test_parse(self):
         names, pathss = zip(*parse("", join(FILE_LOCATION, "..", "resources", "grouped_VAT_09671_Rs_SJakob.svg")))
