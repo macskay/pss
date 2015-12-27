99871e30d145e12c03e8169c9ea10994ffe82b34
Max Klingmann <KlingmannM@gmail.com>
2015-12-27 23:23:02 +0100

    Commit mocked dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import shlex
+from unittest.Mock import MagicMock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
